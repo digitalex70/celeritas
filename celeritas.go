35f15703142810dcf705b281f43abc1a4786bf2f
André Leblanc <a.leblanc@live.com>
2022-02-23 22:09:09 -0500

    initial commit

---
+++ celeritas.go
@@ -0,0 +1,122 @@
+package celeritas
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/digitalex70/celeritas/render"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+const version = "1.0.0"
+
+type Celeritas struct {
+	AppName  string
+	Debug    bool
+	Version  string
+	InfoLog  *log.Logger
+	ErrorLog *log.Logger
+	RootPath string
+	Routes   *chi.Mux
+	Render   *render.Render
+	config   config
+}
+
+type config struct {
+	port     string
+	renderer string
+}
+
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+	err := c.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+
+	err = c.checkDotEnv(rootPath)
+	if err != nil {
+		return err
+	}
+
+	// read .env
+	err = godotenv.Load(rootPath + "/.env")
+	if err != nil {
+		return err
+	}
+	//Create logggers
+	infoLog, errorLog := c.startsLoggger()
+	c.InfoLog = infoLog
+	c.ErrorLog = errorLog
+	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	c.Version = version
+	c.RootPath = rootPath
+	c.Routes = c.routes().(*chi.Mux)
+	c.config = config{
+		port:     os.Getenv("PORT"),
+		renderer: os.Getenv("RENDERER"),
+	}
+	c.createRenderer()
+	return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//ListenAndServe Starts the web server
+func (c *Celeritas) ListenAndServe() {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%s", os.Getenv("PORT")),
+		ErrorLog:     c.ErrorLog,
+		Handler:      c.Routes,
+		IdleTimeout:  30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 600 * time.Second,
+	}
+	c.InfoLog.Printf("Listening on Addr: %s", srv.Addr)
+	err := srv.ListenAndServe()
+	c.ErrorLog.Fatal(err)
+
+}
+
+func (c *Celeritas) checkDotEnv(path string) error {
+	err := c.CreateFileIfNotExist(fmt.Sprintf("%s/.env", path))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) startsLoggger() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile)
+	return infoLog, errorLog
+}
+
+func (c *Celeritas) createRenderer() {
+	myRenderer := render.Render{
+		Renderer: c.config.renderer,
+		RootPath: c.RootPath,
+		Port:     c.config.port,
+	}
+	c.Render = &myRenderer
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/digitalex70/celeritas
+
+go 1.17
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
